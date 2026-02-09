dd6857ff5d4b268148126b7f1f93580260aae42d
ambikeshshukla <medata11423@gmail.com>
2026-02-09 17:04:35 +0530

    User Management

--- src/main/java/com/user/management/aspect/LoggingAspect.java
+++ src/main/java/com/user/management/aspect/LoggingAspect.java
@@ -1,18 +1,26 @@
 package com.user.management.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class LoggingAspect {
 
-    @Before("execution(* com.example.demo.service.*.*(..))")
+    @Before("execution(* com.user.management.service.*.*(..))")
     public void before(JoinPoint joinPoint) {
-        System.out.println("Aspect Method started: "
-                + joinPoint.getSignature().getName());
+        log.info("Aspect Method started: {}", joinPoint.getSignature().getName());
+    }
+
+
+    @AfterReturning("execution(* com.user.management.service.*.*(..))")
+    public void after(JoinPoint joinPoint) {
+        log.info("Aspect Method completed: {}", joinPoint.getSignature().getName());
     }
 
 }
